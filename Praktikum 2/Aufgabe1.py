0329f624d0cdd01dd7dfa7ed3aa9059bd4ad2cd1
Laurenz Dilba <laurenz.dilba@hotmail.de>
2021-05-09 11:16:25 +0200

    Toleranzgrenze hinzugefügt. Entschlüsselung klappt

--- Praktikum 2/Aufgabe1.py
+++ Praktikum 2/Aufgabe1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 
+
 def bitwise_xor_bytes(a, b):
     result_int = int.from_bytes(a, byteorder="big") ^ int.from_bytes(b, byteorder="big")
     return result_int.to_bytes(max(len(a), len(b)), byteorder="big")
@@ -8,41 +9,48 @@ def bitwise_xor_bytes(a, b):
 
 def Decrypt(ciphertext, key):
     encryptedBytes = []
-    with open("data/chiffrat.bin", "rb") as f:
+    with open(ciphertext, "rb") as f:
         while byte := f.read(1):
             encryptedBytes.append(int.from_bytes(byte, "big"))
 
     schluesselstrom = []
-    with open("data/random.dat", "rb") as f:
+    with open(key, "rb") as f:
         while byte := f.read(1):
             schluesselstrom.append(int.from_bytes(byte, "big"))
 
     # print(encryptedBytes)
     start = time.time()
-    for shift in range(10000, 20000):
+    iterations = 2500000
+    toleranzgrenze = 10
+    aktuelltolieriert = toleranzgrenze
+
+    for shift in range(iterations):
         decrypted = []
-        for b, s in zip(encryptedBytes, schluesselstrom[shift::]):
+        valid = True
+        listToSearch = zip(encryptedBytes, schluesselstrom[shift:shift + len(encryptedBytes):])
+        for b, s in listToSearch:
+            res = b ^ s
+            if res < 32 or res > 128:
+                if aktuelltolieriert == 0:
+                    valid = False
+                    break
+                aktuelltolieriert -= 1
+
             decrypted.append(b ^ s)
+        aktuelltolieriert = toleranzgrenze
+        if valid:
+            decryptedText = [chr(x) for x in decrypted]
+            decryptedText = ''.join(decryptedText)
+            end = time.time()
+            print(f'Time elapsed for {shift} iterations: {end - start:.0f}s')
 
-        decryptedText = [chr(x) for x in decrypted]
-        decryptedText = ''.join(decryptedText)
+            return decryptedText
+        else:
+            decrypted.clear()
 
-        if 'and' in decrypted:
-            print(f'found at {shift}')
-            return
-
-        # print(f'Checking: {shift}')
     end = time.time()
-    print(f'Time elapsed for 10000 iterations: {end-start:.0f}s')
-    # print(schluesselstrom)
-    # a = bytes([0x00, 0x01, 0x02, 0x03])
-    # b = bytes([0x03, 0x02, 0x01, 0xff])
-    # print(type(a))
-    # decrypted = bitwise_xor_bytes(a,b)
-    # decrypted = bitwise_xor_bytes(encryptedBytes, schluesselstrom)
-
-    # print(decrypted)
+    print(f'Time elapsed for {iterations} iterations: {end - start:.0f}s')
 
 
 if __name__ == '__main__':
-    Decrypt("a", "b")
+    print(Decrypt("data/chiffrat.bin", "data/random.dat"))
